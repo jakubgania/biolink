4b549e5efa684c8d70fc6168681fea1b1a04dc96
Jakub Gania <contact@jakubgania.io>
2025-03-16 15:48:33 +0100

    development

---
+++ app/link-card.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+
+export function LinkCard({
+  url,
+  title,
+  icon
+}: {
+  url: string,
+  title: string,
+  icon: string
+}) {
+  return (
+    <a href={url} target="_blank" className="group flex items-center w-full rounded-2xl p-1 bg-slate-100 mb-4 hover:bg-slate-50 transition-shadow duration-300">
+      <div className="flex w-full text-center">
+        <div className="flex w-11 h-11 items-center">
+          <Image
+            className="p-1 group-hover:-rotate-[20deg] transition-all ease-in-out duration-150"
+            width={40}
+            height={40}
+            alt={icon}
+            src={icon}
+          />
+        </div>
+        <h2 className="flex justify-center items-center font-normal w-full text-sm md:text-base text-center tracking-wide md:tracking-wider -ml-10">
+          {title}
+        </h2>
+      </div>
+    </a>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import profile from "../public/profile-3.webp"
 import { useEffect } from "react";
 
+import { LinkCard } from "./link-card";
 import { TextCard } from "./text-card";
 
 export default function Home() {
@@ -108,33 +109,4 @@ export default function Home() {
       </main>
     </div>
   );
-}
-
-function LinkCard({
-  url,
-  title,
-  icon
-}: {
-  url: string,
-  title: string,
-  icon: string
-}) {
-  return (
-    <a href={url} target="_blank" className="group flex items-center w-full rounded-2xl p-1 bg-slate-100 mb-4 hover:bg-slate-50 transition-shadow duration-300">
-      <div className="flex w-full text-center">
-        <div className="flex w-11 h-11 items-center">
-          <Image
-            className="p-1 group-hover:-rotate-[20deg] transition-all ease-in-out duration-150"
-            width={40}
-            height={40}
-            alt={icon}
-            src={icon}
-          />
-        </div>
-        <h2 className="flex justify-center items-center font-normal w-full text-sm md:text-base text-center tracking-wide md:tracking-wider -ml-10">
-          {title}
-        </h2>
-      </div>
-    </a>
-  )
 }
